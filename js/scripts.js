07464e84100ca81c6fb83da8d6133fdf37662ed5
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-01-28 10:51:00 -0800

    add language result selection logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,5 +4,20 @@ $(document).ready(function(event) {
     const identifyWith = $("#identify-with").val();
     const favoriteColor = $("#favorite-color").val();
     const preferredOrganization = $("#preferred-organization").val();
+
+    // unhides ruby language result
+    if (identifyWith === "precious-gems" && favoriteColor === "red") {
+      $('#ruby').show();
+    } 
+    // unhides python language result
+    else if (identifyWith === "snakes" && favoriteColor === "green") {
+      $('#python').show();
+    } 
+    // unhides javascript language
+    else if (identifyWith === "aesthetics" && favoriteColor === "yellow") {
+      $('#javascript').show();
+    } else {
+      $('#javascript').show();
+    }
   });
 });
