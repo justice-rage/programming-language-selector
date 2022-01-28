9355e1f7918c388b2ce1497f0069d8fb95dd880a
Justice Douglas <64440410+mr-justice@users.noreply.github.com>
2022-01-28 10:21:02 -0800

    add result variables utilizing lower camel case

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(event) {
+
+  $("form#quiz").submit(function(event) {
+    const languageRuby = $("#ruby").val();
+    const languagePython = $("#python").val();
+    const languageJavaScript = $("#javascript").val();
+  });
+});
